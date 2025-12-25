a4131a86fdd5d6823baaa60ce230b61339f0dd1d
Mrinal Sharma <mrinalwork2@gmail.com>
2025-12-25 18:34:12 -0500

    bank_webhook_service

---
+++ apps/bank_webhook/src/index.ts
@@ -0,0 +1,61 @@
+import express from "express";
+import { prisma } from "@repo/db/";
+
+const app = express();
+
+app.post("/hdfcWebhook", async (req, res) => {
+    //TODO: Add zod validation here?
+        //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+
+    const paymentInformation : {token : string, userId: string, amount: string} = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    };
+    await prisma.balance.update ({
+        where: { 
+            userId: Number(paymentInformation.userId) 
+        },
+        data: {
+            amount: {
+                increment: Number(paymentInformation.amount)    
+            }
+        }
+    })
+
+    try {
+        await prisma.$transaction([
+            prisma.balance.updateMany({
+                where: {
+                    userId: Number(paymentInformation.userId)
+                },
+                data: {
+                    amount: {
+                        // You can also get this from your DB
+                        increment: Number(paymentInformation.amount)
+                    }
+                }
+            }),
+            prisma.onRampTransaction.updateMany({
+                where: {
+                    token: paymentInformation.token
+                }, 
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+
+        res.json({
+            message: "Captured"
+        })
+    }catch(e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
+
+})
+    // Update balance in db, add txn record
+    app.listen(3003);
