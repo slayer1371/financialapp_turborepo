823c9cda596c5b8a0195358e840d83719e3d95b2
Mrinal Sharma <mrinalwork2@gmail.com>
2025-12-26 20:36:12 -0500

    bank webhook service

--- apps/bank_webhook/src/index.ts
+++ apps/bank_webhook/src/index.ts
@@ -1,28 +1,44 @@
 import express from "express";
-import { prisma } from "@repo/db/";
+import { prisma } from "@repo/db";
 
 const app = express();
+
+app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
     //TODO: Add zod validation here?
         //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
 
+        /*
+        const pi = {
+        token : req.body.token}
+
+        */
+    
     const paymentInformation : {token : string, userId: string, amount: string} = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     };
-    await prisma.balance.update ({
-        where: { 
-            userId: Number(paymentInformation.userId) 
-        },
-        data: {
-            amount: {
-                increment: Number(paymentInformation.amount)    
-            }
+
+    const status  = await prisma.onRampTransaction.findFirst({
+        where : {
+            token : paymentInformation.token
         }
     })
 
+    if(!status) {
+        return res.status(400).json({
+            message : "Invalid token"
+        })
+    }
+
+    if(status.status === "Success") {
+        return  res.json({
+            message : "Already processed"
+        })
+    }
+    
     try {
         await prisma.$transaction([
             prisma.balance.updateMany({
